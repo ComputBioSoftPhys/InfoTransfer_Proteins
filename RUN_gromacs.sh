abb4b1e26cc293a9a6321fa4627e88b4c13f12e3
Anil Kumar Sahoo <201992kumarsahoo@gmail.com>
2025-08-07 21:31:44 +0200

    Update RUN_gromacs.sh

--- RUN_gromacs.sh
+++ RUN_gromacs.sh
@@ -12,7 +12,7 @@ gmx mdrun -deffnm npt -pin on -nt 8
 gmx grompp -f md.mdp -c npt.gro -t npt.cpt -p topol.top -o md_10ns.tpr
 gmx mdrun -deffnm md_10ns -pin on -nt 8
 
-for((s=10;s<=20000;s=s+10))
+for((s=20;s<=20000;s=s+10))
 do
    rstpt=$((s - 10))
    gmx convert-tpr -s md_${rstpt}ns.tpr -o md_${s}ns.tpr -extend 10000
